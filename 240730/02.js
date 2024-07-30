34f4dd532d1d6be745b109c776b80467d43bae0c
doo1b <tngml928@gmail.com>
2024-07-30 12:31:20 +0900

    test

---
+++ 240730/02.js
@@ -0,0 +1,25 @@
+// async await 를 사용한 코드로 변경해보기
+const API_URL = "https://open.api.com/v1/data";
+const WRONG_URL = "https://open.api.com/v1/wrong";
+
+const promis = async (url) => {
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if ( url === API_URL ) {
+            resolve("성공")
+        } else reject ("실패")
+    }, 3000);
+})
+}
+
+async function getData(url) {
+    try {
+    const answer = await promis(url);
+    console.log(answer);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+getData(API_URL);
+getData(WRONG_URL);
