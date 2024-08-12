1da57a1a0f631100cb9e501285b7754879e07eec
doo1b <tngml928@gmail.com>
2024-08-12 22:24:22 +0900

    fix

--- 240730/02.js
+++ 240730/02.js
@@ -3,23 +3,23 @@ const API_URL = "https://open.api.com/v1/data";
 const WRONG_URL = "https://open.api.com/v1/wrong";
 
 function promise(url) {
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-        if ( url === API_URL ) {
-            resolve("성공")
-        } else reject ("실패")
+      if (url === API_URL) {
+        resolve("성공");
+      } else reject("실패");
     }, 3000);
-});
+  });
 }
 
 async function getData(url) {
-    try {
+  try {
     const answer = await promise(url);
     console.log(answer);
-    } catch (error) {
-        console.error(error);
-    }
-};
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 getData(API_URL);
 getData(WRONG_URL);
